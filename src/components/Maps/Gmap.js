a926b3fc79b152ffef930814f769b8a713a3b210
shunpingong <90850278+shunpingong@users.noreply.github.com>
2024-04-09 23:53:47 +0800

    Update Gmap.js

--- src/components/Maps/Gmap.js
+++ src/components/Maps/Gmap.js
@@ -73,21 +73,22 @@ function Gmap() {
       console.error(`Your browser doesn't support Geolocation`);
     }
   }
-  useEffect(() => {
-    // Watch for changes in geolocation
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setCenter({ lat: latitude, lng: longitude }); // Update the center of the map
-      },
-      (error) => {
-        console.error(`Error getting geolocation: ${error.message}`);
-      }
-    );
-    return () => {
-      navigator.geolocation.clearWatch(watchId);
-    };
-  }, []);
+
+  // useEffect(() => {
+  //   // Watch for changes in geolocation
+  //   const watchId = navigator.geolocation.watchPosition(
+  //     (position) => {
+  //       const { latitude, longitude } = position.coords;
+  //       setCenter({ lat: latitude, lng: longitude }); // Update the center of the map
+  //     },
+  //     (error) => {
+  //       console.error(`Error getting geolocation: ${error.message}`);
+  //     }
+  //   );
+  //   return () => {
+  //     navigator.geolocation.clearWatch(watchId);
+  //   };
+  // }, []);
 
   useEffect(() => {
     readFavouriteCarparks()
